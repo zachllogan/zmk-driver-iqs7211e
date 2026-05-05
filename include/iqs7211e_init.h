584cacf102258eb100580cc78980f5856427147c
Zach Logan <zachllogan@gmail.com>
2026-05-05 12:34:28 -0600

    Update timeout constants for idle and LP1 modes

--- include/iqs7211e_init.h
+++ include/iqs7211e_init.h
@@ -70,9 +70,9 @@
 #define ACTIVE_MODE_TIMEOUT_1                    0x00
 #define IDLE_TOUCH_MODE_TIMEOUT_0                0x3C
 #define IDLE_TOUCH_MODE_TIMEOUT_1                0x00
-#define IDLE_MODE_TIMEOUT_0                      0x00
+#define IDLE_MODE_TIMEOUT_0                      0x0A
 #define IDLE_MODE_TIMEOUT_1                      0x00
-#define LP1_MODE_TIMEOUT_0                       0x00
+#define LP1_MODE_TIMEOUT_0                       0x0A
 #define LP1_MODE_TIMEOUT_1                       0x00
 #define REATI_RETRY_TIME                         0x05
 #define REF_UPDATE_TIME                          0x08
